3ea3e0cd8502cb17d92f61600f77032bb497de3a
Max Lahey <maxwellslahey@gmail.com>
2013-04-07 09:20:50 -0400

    dry'd up comparators more

--- lib/specifind/comparators/base.rb
+++ lib/specifind/comparators/base.rb
@@ -22,6 +22,21 @@ module Specifind
         nil
       end
 
+      def self.comparators_data
+        [
+          ['_in_list',              1, %w(_list),                     Proc.new{|v| "in (#{v[0]})"}],
+          ['_less_than_equals',     1, %w(_val),                      Proc.new{|v| "<= #{v[0]}"}],
+          ['_less_than',            1, %w(_val),                      Proc.new{|v| "< #{v[0]}"}],
+          ['_greater_than_equals',  1, %w(_val),                      Proc.new{|v| ">= #{v[0]}"}],
+          ['_greater_than',         1, %w(_val),                      Proc.new{|v| "> #{v[0]}"}],
+          ['_not_equal',            1, %w(_val),                      Proc.new{|v| "!= #{v[0]}"}],
+          ['_between',              2, %w(_bound_one _bound_two),     Proc.new{|v| "between #{v[0]} and #{v[1]}"}],
+          ['_is_not_null',          0, [],                            Proc.new{|v| "is not null"}],
+          ['_is_null',              0, [],                            Proc.new{|v| "is null"}],
+          ['_equals',               1, %w(_val),                      Proc.new{|v| "= #{v[0]}"}]
+        ]
+      end
+
       # From the data listed in the Comparator class definition, Comparator.generate_comparators constructs each type of Comparators
       # and adds it to Comparator.comparators
       def self.generate_comparators
--- lib/specifind/comparators/postgre_sql.rb
+++ lib/specifind/comparators/postgre_sql.rb
@@ -7,19 +7,9 @@ module Specifind
     class PostgreSQL < Base
       
       def self.comparators_data
-        [
-          ['_in_list',              1, %w(_list),                     Proc.new{|v| "in (#{v[0]})"}],
-          ['_less_than_equals',     1, %w(_val),                      Proc.new{|v| "<= #{v[0]}"}],
-          ['_less_than',            1, %w(_val),                      Proc.new{|v| "< #{v[0]}"}],
-          ['_greater_than_equals',  1, %w(_val),                      Proc.new{|v| ">= #{v[0]}"}],
-          ['_greater_than',         1, %w(_val),                      Proc.new{|v| "> #{v[0]}"}],
+        super + [
           ['_like',                 1, %w(_val),                      Proc.new{|v| "like #{v[0]}"}],
-          ['_ilike',                1, %w(_val),                      Proc.new{|v| "ilike #{v[0]}"}],
-          ['_not_equal',            1, %w(_val),                      Proc.new{|v| "!= #{v[0]}"}],
-          ['_between',              2, %w(_bound_one _bound_two),     Proc.new{|v| "between #{v[0]} and #{v[1]}"}],
-          ['_is_not_null',          0, [],                            Proc.new{|v| "is not null"}],
-          ['_is_null',              0, [],                            Proc.new{|v| "is null"}],
-          ['_equals',               1, %w(_val),                      Proc.new{|v| "= #{v[0]}"}]
+          ['_ilike',                1, %w(_val),                      Proc.new{|v| "ilike #{v[0]}"}]
         ]
       end
 
